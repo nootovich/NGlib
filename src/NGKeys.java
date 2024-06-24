c356e81a63551fb9868c39d5288765c461257d5e
nootovich <mailnonamer@gmail.com>
2024-06-24 10:45:52 +0300

    Made a draft of a key-list

---
+++ src/NGKeys.java
@@ -0,0 +1,101 @@
+@SuppressWarnings({"unused", "SpellCheckingInspection"})
+public class NGKeys {
+    /* TODO:
+     *  4  Arrows
+     *  2  Ctrl
+     *  2  Shift
+     *  2  Alt
+     *  1? Meta
+     *  1  Insert
+     *  1  Delete
+     *  1  Home
+     *  1  End
+     *  1  PageUp
+     *  1  PageDn
+     *  1  PrintScreen
+     *  1  PauseBreak
+     *  1  NumLock
+     *  16 Num-keys
+     *  1  CapsLock
+     *  1  ScrollLock
+     *  24 F-keys
+     *  total :62 */
+
+    // 0-7 vacant
+    public static final int BACKSPACE = 8;
+    public static final int TAB       = 9;
+    public static final int ENTER     = 10;
+    // 11-13 vacant
+    public static final int LSHIFT    = 14;
+    public static final int RSHIFT    = 15;
+    // 16-26 vacant
+    public static final int ESCAPE    = 27;
+    // 28-31 vacant
+    public static final int SPACEBAR  = 32;
+    // 33 -> 49
+    // 34 -> 39
+    // 35 -> 51
+    // 36 -> 52
+    // 37 -> 53
+    // 38 -> 55
+    public static final int QUOTE     = 39;
+    // 40 -> 57
+    // 41 -> 48
+    // 42 -> 56
+    // 43 -> 61
+    public static final int COMMA     = 44;
+    public static final int MINUS     = 45;
+    public static final int PERIOD    = 46;
+    public static final int SLASH     = 47;
+    public static final int _0        = 48;
+    public static final int _1        = 49;
+    public static final int _2        = 50;
+    public static final int _3        = 51;
+    public static final int _4        = 52;
+    public static final int _5        = 53;
+    public static final int _6        = 54;
+    public static final int _7        = 55;
+    public static final int _8        = 56;
+    public static final int _9        = 57;
+    // 58 -> 59
+    public static final int SEMICOLON = 59;
+    // 60 -> 44
+    public static final int EQUALS    = 61;
+    // 62 -> 46
+    // 63 -> 47
+    // 64 -> 50
+    public static final int A         = 65;
+    public static final int B         = 66;
+    public static final int C         = 67;
+    public static final int D         = 68;
+    public static final int E         = 69;
+    public static final int F         = 70;
+    public static final int G         = 71;
+    public static final int H         = 72;
+    public static final int I         = 73;
+    public static final int J         = 74;
+    public static final int K         = 75;
+    public static final int L         = 76;
+    public static final int M         = 77;
+    public static final int N         = 78;
+    public static final int O         = 79;
+    public static final int P         = 80;
+    public static final int Q         = 81;
+    public static final int R         = 82;
+    public static final int S         = 83;
+    public static final int T         = 84;
+    public static final int U         = 85;
+    public static final int V         = 86;
+    public static final int W         = 87;
+    public static final int X         = 88;
+    public static final int Y         = 89;
+    public static final int Z         = 90;
+    public static final int LBRACKET  = 91;
+    public static final int BACKSLASH = 92;
+    public static final int RBRACKET  = 93;
+    // 94 -> 54
+    // 95 -> 45
+    public static final int BACKTICK  = 96;
+    // 97-125 -> 65-93
+    // 126 -> 96
+}
