01c757f65af63dfe70beaafa93ccf25ef959b936
nootovich <mailnonamer@gmail.com>
2024-08-13 15:37:49 +0300

    Some slight improvements
    Wasn't able to make inner classes reloadable though :(

--- src/nootovich/nglib/NGHotReloadable.java
+++ src/nootovich/nglib/NGHotReloadable.java
@@ -4,17 +4,17 @@ import com.sun.tools.javac.Main;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Path;
 
 // IMPORTANT NOTE: Hot reloadable classes will break after reload if they encounter a `protected` field
 
 public class NGHotReloadable extends ClassLoader {
 
-    // TODO: un-hardcode again...
-    public String sourceFile;
-    public String sourcePath = ".\\src\\";
-    public String classPath  = ".\\out\\production\\snake\\";
+    public static final String CLASSPATH = Path.of(System.getProperties().get("java.class.path").toString().split(";")[0]).getParent().toString() + "\\";
 
-    public long lastModifiedTime = 0;
+    public String className;
+    public String sourceFile;
+    public long   lastModifiedTime = 0;
 
     public NGHotReloadable() {
         super(NGHotReloadable.class.getClassLoader());
@@ -25,46 +25,45 @@ public class NGHotReloadable extends ClassLoader {
         Class<?> declaringClass = getClass();
         while (declaringClass.getDeclaringClass() != null) declaringClass = declaringClass.getDeclaringClass();
 
+        className = declaringClass.getName();
         String sourceFileName = declaringClass.getSimpleName() + ".java";
-        sourceFile = NGFileSystem.getParent(NGFileSystem.findRecursively(sourceFileName)) + sourceFileName;
+        sourceFile = NGFileSystem.findRecursively(sourceFileName);
     }
 
     public NGHotReloadable reloadIfNeeded() {
-        long curModifiedTime = NGFileSystem.getLastModifiedTime(sourceFile);
+        NGHotReloadable result          = null;
+        long            curModifiedTime = NGFileSystem.getLastModifiedTime(sourceFile);
         if (curModifiedTime != lastModifiedTime) {
-            if (lastModifiedTime > 0) return getNew();
+            if (lastModifiedTime > 0) result = getNew();
             lastModifiedTime = curModifiedTime;
         }
-        return null;
+        return result;
     }
 
     @Override
-    public Class<?> loadClass(String name) {
-        if (!name.equals(getClass().getName())) try {
-            return super.loadClass(name);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        byte[] classData = NGFileSystem.loadBytes(classPath + getClass().getName().replace('.', '/') + ".class");
-        return defineClass(getClass().getName(), classData, 0, classData.length);
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        if (!name.equals(getClass().getName())) return super.loadClass(name);
+        byte[] classData = NGFileSystem.loadBytes(CLASSPATH + className.replace('.', '/') + ".class");
+        return defineClass(className, classData, 0, classData.length);
     }
 
     // CREDIT: https://jenkov.com/tutorials/java-reflection/dynamic-class-loading-reloading.html
     public NGHotReloadable getNew() {
-        try {
-            PrintStream stderr = System.err;
-            System.setErr(new PrintStream(OutputStream.nullOutputStream()));
-            int compilationResult = Main.compile(new String[]{"-d", classPath, "-sourcepath", sourcePath, sourceFile});
+        try { // TODO: don't like the look of this function. I'm glad that it works though
+            PrintStream   stderr = System.err;
+            StringBuilder sb     = new StringBuilder();
+            System.setErr(new PrintStream(new OutputStream() { public void write(int b) { sb.append((char) b); } }));
+            int compilationResult = Main.compile(new String[]{"-d", CLASSPATH, sourceFile});
             System.setErr(stderr);
-            if (compilationResult != 0) return NGUtils.info("Compilation failed!");
-            NGUtils.info("Reloaded class: " + getClass().getName());
+            if (compilationResult != 0) return NGUtils.info("Compilation failed!\n" + sb);
+            NGUtils.info("Reloaded class: " + className);
             return (NGHotReloadable) loadClass(getClass().getName()).getDeclaredConstructor().newInstance();
         } catch (InstantiationException
                  | IllegalAccessException
                  | InvocationTargetException
-                 | NoSuchMethodException e) {
-            // TODO: Actually handle errors
-            throw new RuntimeException(e);
+                 | NoSuchMethodException
+                 | ClassNotFoundException e) {
+            throw new RuntimeException(e); // TODO: Actually handle errors
         }
     }
 }
