0da1aa26be137e9c2aadd326b1bcb199c6e169e5
nootovich <mailnonamer@gmail.com>
2024-11-07 11:48:41 +0300

    Added base64 decoding

--- src/nootovich/nglib/NGBits.java
+++ src/nootovich/nglib/NGBits.java
@@ -1,20 +1,54 @@
 package nootovich.nglib;
 
+import java.util.ArrayList;
+
 import static nootovich.nglib.NGUtils.error;
 
 public class NGBits {
 
     public static final int BYTE_SIZE = 8;
 
-    public boolean[] data;
+    public boolean[] data    = new boolean[]{ };
     public int       pointer = 0;
+
+    public NGBits() { }
 
     public NGBits(boolean[] data) {
         this.data = data;
     }
 
     public NGBits(byte[] data) {
-        this.data = NGConvert.getBits(data);
+        this.data = getBits(data);
+    }
+
+    public NGBits(byte data) {
+        this.data = getBits(data);
+    }
+
+    public NGBits(String data) {
+        this.data = getBits(data.getBytes());
+    }
+
+    public NGBits(ArrayList<NGBits> data) {
+        for (NGBits bits: data) {
+            push(bits.data);
+        }
+    }
+
+    public static boolean[] getBits(byte n) {
+        boolean[] bits = new boolean[BYTE_SIZE];
+        for (int i = 0; i < bits.length; i++) {
+            bits[BYTE_SIZE - i - 1] = (n & (1 << i)) > 0;
+        }
+        return bits;
+    }
+
+    public static boolean[] getBits(byte[] n) {
+        boolean[] bits = new boolean[BYTE_SIZE * n.length];
+        for (int i = 0; i < n.length; i++) {
+            System.arraycopy(getBits(n[i]), 0, bits, BYTE_SIZE * i, BYTE_SIZE);
+        }
+        return bits;
     }
 
     public NGBits chop(int n) {
@@ -23,6 +57,41 @@ public class NGBits {
         System.arraycopy(data, pointer, result, 0, n);
         pointer += n;
         return new NGBits(result);
+    }
+
+    public boolean chop1() {
+        if (length() < 1) error("TODO: error message"); // TODO:
+        return data[pointer++];
+    }
+
+    public boolean[] pop(int n) {
+        if (length() < n) error("TODO: error message"); // TODO:
+        boolean[] result = new boolean[n];
+        System.arraycopy(data, pointer, result, 0, n);
+        boolean[] temp = data;
+        data = new boolean[data.length - n];
+        System.arraycopy(temp, 0, data, 0, data.length);
+        return result;
+    }
+
+    public void push(boolean bit) {
+        int    len  = length();
+        NGBits copy = copy();
+        data = new boolean[len + 1];
+        System.arraycopy(copy.data, 0, data, 0, len);
+        data[len] = bit;
+    }
+
+    public void push(boolean[] bits) {
+        int    len  = length();
+        NGBits copy = copy();
+        data = new boolean[len + bits.length];
+        System.arraycopy(copy.data, 0, data, 0, len);
+        System.arraycopy(bits, 0, data, len, bits.length);
+    }
+
+    public void push(byte n) {
+        push(getBits(n));
     }
 
     public byte toByte() {
--- src/nootovich/nglib/NGConvert.java
+++ src/nootovich/nglib/NGConvert.java
@@ -1,13 +1,14 @@
 package nootovich.nglib;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 import static nootovich.nglib.NGBits.BYTE_SIZE;
 
 public class NGConvert {
 
     private static final int    BASE64_CHAR_SIZE = 6;
-    private static final char[] BASE64_TABLE     = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".toCharArray();
+    private static final String BASE64_TABLE     = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     private static final char   BASE64_PADDING   = '=';
 
     public static String encodeBase64(String data) {
@@ -19,25 +20,28 @@ public class NGConvert {
         NGBits        bits   = new NGBits(data);
         while (bits.length() > 0) {
             NGBits slice = bits.chop(Math.min(BASE64_CHAR_SIZE, bits.length()));
-            slice.rightPad(6);
-            result.append(BASE64_TABLE[slice.toByte()]);
+            slice.rightPad(BASE64_CHAR_SIZE);
+            result.append(BASE64_TABLE.charAt(slice.toByte()));
         }
         return result.toString();
     }
 
-    public static boolean[] getBits(byte n) {
-        boolean[] bits = new boolean[BYTE_SIZE];
-        for (int i = 0; i < bits.length; i++) {
-            bits[BYTE_SIZE - i - 1] = (n & (1 << i)) > 0;
+    public static byte[] decodeBase64(String data) {
+        ArrayList<Byte> result = new ArrayList<>();
+        NGBits          bits   = new NGBits();
+        for (int i = 0; i < data.length(); i++) {
+            NGBits slice = new NGBits((byte) BASE64_TABLE.indexOf(data.charAt(i)));
+            slice.chop(2);
+            bits.push(slice.copy().data);
         }
-        return bits;
-    }
-
-    public static boolean[] getBits(byte[] n) {
-        boolean[] bits = new boolean[BYTE_SIZE * n.length];
-        for (int i = 0; i < n.length; i++) {
-            System.arraycopy(getBits(n[i]), 0, bits, BYTE_SIZE * i, BYTE_SIZE);
+        while (bits.length() > 0) {
+            NGBits slice = bits.chop(Math.min(BYTE_SIZE, bits.length()));
+            if (slice.length() < BYTE_SIZE && slice.toByte() == 0) break;
+            slice.rightPad(BYTE_SIZE);
+            result.add(slice.toByte());
         }
-        return bits;
+        byte[] ret = new byte[result.size()];
+        for (int i = 0; i < result.size(); i++) ret[i] = result.get(i);
+        return ret;
     }
 }
